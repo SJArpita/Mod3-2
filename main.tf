c6f7771eda451dac3fc76cc78de52d085dfccf7d
SJArpita <rsheea12@gmail.com>
2025-04-05 12:25:28 +0800

    changing main

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 provider "aws" {
   region = "us-east-1"
+
 }
 
 terraform {
@@ -7,16 +8,28 @@ terraform {
     bucket = "sctp-ce9-tfstate"
     key    = "arpita-ce9-module3-lesson2.tfstate" #Change this
     region = "us-east-1"
+    
+  }
+
+  required_version = ">= 1.1.7"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.94.1"
+    }
   }
 }
 
 data "aws_caller_identity" "current" {}
 
 locals {
-  name_prefix = split("/", "${data.aws_caller_identity.current.arn}")[1]
+  name_prefix = split("/", data.aws_caller_identity.current.arn)[1]
   account_id  = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_s3_bucket" "s3_tf" {
   bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
+ 
+
 }
