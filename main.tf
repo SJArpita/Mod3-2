3420295a60156e9397ac792d9571f152f98c6fbe
SJArpita <rsheea12@gmail.com>
2025-04-05 13:04:31 +0800

    changing main

--- main.tf
+++ main.tf
@@ -31,12 +31,12 @@ locals {
 }
 
 resource "aws_s3_bucket" "s3_tf" {
-  #  checkov:skip=CKV_AWS_62
-  #   checkov:skip=CKV_AWS_145
-  #   checkov:skip=CKV_AWS_18
-  #   checkov:skip=CKV_AWS_144
-  #   checkov:skip=CKV_AWS_61
-  #   checkov:skip=CKV_AWS_6
+  #checkov:skip=CKV_AWS_62
+  #checkov:skip=CKV_AWS_145
+  #checkov:skip=CKV_AWS_18
+  #checkov:skip=CKV_AWS_144
+  #checkov:skip=CKV_AWS_61
+  #checkov:skip=CKV_AWS_6
   bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
 
 }
